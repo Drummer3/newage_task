ee4dae4d1ce0132f9759ae3fdc9bdac103a8f679
Drummer3 <drummerthree@gmail.com>
2022-11-08 01:30:50 +0400

    add email validation

--- middleware/authRouter.js
+++ middleware/authRouter.js
@@ -4,6 +4,28 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
+const isBirthdayValid = (birthday) => {
+	if (birthday.length !== 10 || !/\d{4}-\d{2}-\d{2}/.test(birthday))
+		return [
+			false,
+			"Birthday format isn't valid. Please use following format: YYYY-MM-DD",
+		];
+
+	const date = new Date(birthday);
+	if (!date.getDate()) return false, "Birthday value isn't valid";
+
+	if (date.getFullYear() < 1920)
+		return [false, "Birthday shouldn't be older than 1920-01-01"];
+
+	const differenceYears = Math.floor(
+		Math.abs(new Date() - date) / (1000 * 60 * 60 * 24 * 365)
+	);
+	if (differenceYears < 18)
+		return [false, "User should be at least 18 years old"];
+
+	return [true];
+};
+
 router.post("/sign-up", (req, res) => {
 	const firstName = req.body.firstName;
 	const lastName = req.body.lastName;
@@ -26,6 +48,10 @@ router.post("/sign-up", (req, res) => {
 				error: "Last name shouldn't only use latin characters",
 			})
 		);
+
+	const [isValid, error] = isBirthdayValid(birthday);
+	if (!isValid)
+		return res.status(400), res.json({ error });
 
 	if (!/^[A-Za-z0-9._%+-]+@newage\.io$/.test(email))
 		return (
