da5f88e1dfd9dbe3d591c12dadbe75f7d9648d41
Drummer3 <drummerthree@gmail.com>
2022-11-08 11:40:47 +0400

    fix router import

--- node_js/index.js
+++ node_js/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const authRouter = require("./middleware/authRouter");
+const authRouter = require("./routes/authRouter");
 
 app.use(express.json());
 app.use("/api/auth", authRouter);
