bb4f136377285a91206adbbfdb1e18de9d78bb97
Drummer3 <drummerthree@gmail.com>
2022-11-08 16:43:33 +0400

    separate router for /auth and /users

--- node_js/index.js
+++ node_js/index.js
@@ -3,10 +3,11 @@ const app = express();
 const port = 3000;
 
 const authRouter = require("./routes/authRouter");
+const userRouter = require("./routes/userRouter");
 
 app.use(express.json());
 app.use("/api/auth", authRouter);
-
+app.use("/api/users", userRouter);
 app.use((req, res) => (res.status(404), res.json()));
 
 app.listen(port, () => {
