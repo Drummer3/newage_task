be4f445f5414fa5c25bf571538294206fbd19333
Drummer3 <drummerthree@gmail.com>
2022-11-08 14:18:01 +0400

    convert database to indexed database

--- node_js/utils/database.js
+++ node_js/utils/database.js
@@ -4,19 +4,23 @@ const { v4: uuidv4 } = require("uuid");
 const DATABASE_FILE = "./database/users.json";
 
 function writeUserToDatabase(userDetails) {
-	userDetails.uuid = uuidv4();
+	const userId = uuidv4();
+	userDetails.uuid = userId;
 	const existingFile = fs.readFileSync(DATABASE_FILE);
 	const existingData = JSON.parse(existingFile);
-	existingData.push(userDetails);
+	existingData[userId] = userDetails;
 	fs.writeFileSync(DATABASE_FILE, JSON.stringify(existingData));
 	return userDetails;
 }
 
 function readUserFromDatabase(email) {
 	const database = JSON.parse(fs.readFileSync(DATABASE_FILE));
-	const userDetails = database.filter((record) => record.email === email);
-	if (userDetails) return userDetails[0];
-	return {};
+	const keys = Object.keys(database);
+	let userDetails = null;
+	keys.forEach((user) => {
+		if (database[user].email === email) userDetails = database[user];
+	});
+	return userDetails;
 }
 
 module.exports = { writeUserToDatabase, readUserFromDatabase };
