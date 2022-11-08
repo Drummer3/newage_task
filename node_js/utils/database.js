207d61e13638d8aaf67ec716c236261b28a37e6b
Drummer3 <drummerthree@gmail.com>
2022-11-08 13:05:09 +0400

    add readUserFromDatabase function and move database path to variable

--- node_js/utils/database.js
+++ node_js/utils/database.js
@@ -1,12 +1,22 @@
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
+const DATABASE_FILE = "./database/users.json";
+
 function writeUserToDatabase(userDetails) {
 	userDetails.uuid = uuidv4();
-	const existingFile = fs.readFileSync("./database/users.json");
+	const existingFile = fs.readFileSync(DATABASE_FILE);
 	const existingData = JSON.parse(existingFile);
 	existingData.push(userDetails);
-	fs.writeFileSync("./database/users.json", JSON.stringify(existingData));
+	fs.writeFileSync(DATABASE_FILE, JSON.stringify(existingData));
+	return userDetails;
 }
 
-module.exports = { writeUserToDatabase };
+function readUserFromDatabase(email) {
+	const database = JSON.parse(fs.readFileSync(DATABASE_FILE));
+	const userDetails = database.filter((record) => record.email === email);
+	if (userDetails) return userDetails[0];
+	return {};
+}
+
+module.exports = { writeUserToDatabase, readUserFromDatabase };
