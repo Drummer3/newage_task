4fc9a66b7cb6d9c8267b9aaebb5778ed51cebb92
Drummer3 <drummerthree@gmail.com>
2022-11-09 17:15:29 +0400

    add headers to express app

--- node_js/index.js
+++ node_js/index.js
@@ -4,6 +4,12 @@ const port = 8000;
 
 const authRouter = require("./routes/authRouter");
 const userRouter = require("./routes/userRouter");
+app.all("*", function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	next();
+});
 
 app.use(express.json());
 app.use("/api/auth", authRouter);
