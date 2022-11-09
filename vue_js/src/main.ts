e2ee8bcb130c8738634e3cb0dbd082bc037d5522
Drummer3 <drummerthree@gmail.com>
2022-11-10 01:04:04 +0400

    improve store naming

--- vue_js/src/main.ts
+++ vue_js/src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import authStore from "./store/authStore";
+import store from "./store/authStore";
 import "./assets/main.css";
 
 const app = createApp(App);
 
 app.use(router);
-app.use(authStore);
+app.use(store);
 app.mount("#app");
