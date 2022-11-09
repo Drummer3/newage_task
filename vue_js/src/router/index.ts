567a122000d1c4fa653d6db0d034ec689103153c
Drummer3 <drummerthree@gmail.com>
2022-11-10 01:08:56 +0400

    create /profile route

--- vue_js/src/router/index.ts
+++ vue_js/src/router/index.ts
@@ -27,6 +27,14 @@ const router = createRouter({
       },
       component: () => import("../views/SignInView.vue"),
     },
+    {
+      path: "/profile",
+      name: "profile",
+      beforeEnter: () => {
+        if (!authStore.state.authenticated) return false;
+      },
+      component: () => import("../views/ProfileView.vue"),
+    },
   ],
 });
 
