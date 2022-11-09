121e6046acf908082e81f50ef4ea32c0fcb6ba7b
Drummer3 <drummerthree@gmail.com>
2022-11-09 17:58:19 +0400

    header styling

--- vue_js/src/App.vue
+++ vue_js/src/App.vue
@@ -6,10 +6,20 @@ export default {
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/sign-up">Sign Up</RouterLink>
-      <RouterLink to="/sign-in">Sign In</RouterLink>
+  <header class="w-full bg-gray-50 mb-4 p-4 rounded-b-3xl">
+    <nav class="flex gap-4">
+      <RouterLink
+        class="px-4 py-2 duration-150 hover:text-purple-700"
+        to="/sign-up"
+      >
+        Sign Up
+      </RouterLink>
+      <RouterLink
+        class="px-4 py-2 duration-150 hover:text-purple-700"
+        to="/sign-in"
+      >
+        Sign In
+      </RouterLink>
     </nav>
   </header>
 
