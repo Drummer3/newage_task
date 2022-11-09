e5bee0105c879ba639bda7a7de1195fa11fa38d7
Drummer3 <drummerthree@gmail.com>
2022-11-10 01:36:32 +0400

    allow receiving authorization token

--- node_js/index.js
+++ node_js/index.js
@@ -7,7 +7,7 @@ const userRouter = require("./routes/userRouter");
 app.all("*", function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-	res.header("Access-Control-Allow-Headers", "Content-Type");
+	res.header("Access-Control-Allow-Headers", ["Content-Type", "Authorization"]);
 	next();
 });
 
