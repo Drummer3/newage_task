4968409e383f2c58ce9b7da3ddc74e0dfdaf06c4
Drummer3 <drummerthree@gmail.com>
2022-11-10 12:13:06 +0400

    form field and button component classes

--- vue_js/src/views/SignInView.vue
+++ vue_js/src/views/SignInView.vue
@@ -42,32 +42,21 @@ export default {
   >
     <p class="text-center text-xl">Sign In</p>
     <div v-if="error" class="error-box">
-      <span class="text-sm font-semibold">{{ error }}</span>
+      <span>{{ error }}</span>
     </div>
-    <label>
-      <p class="text-xs">Email</p>
-      <input
-        required
-        class="w-full bg-gray-800 rounded-lg px-4 py-2 duration-100 text-gray-50 focus:outline-none focus:bg-gray-700"
-        type="email"
-        v-model="email"
-      />
+    <label class="form-field">
+      <p>Email</p>
+      <input required type="email" v-model="email" />
     </label>
-    <label>
-      <p class="text-xs">Password</p>
+    <label class="form-field">
+      <p>Password</p>
       <input
         required
-        class="w-full bg-gray-800 rounded-lg px-4 py-2 duration-100 text-gray-50 focus:outline-none focus:bg-gray-700"
         type="password"
         placeholder="p4ssw0rd"
         v-model="password"
       />
     </label>
-    <button
-      class="w-full px-4 py-2 rounded-lg text-gray-50 bg-purple-600 duration-100 hover:bg-purple-500"
-      type="submit"
-    >
-      Sign In
-    </button>
+    <button class="button button__purple" type="submit">Sign In</button>
   </form>
 </template>
